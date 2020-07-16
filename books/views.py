f416d58b4f5c965a3cdf650b63e0778b648be1a0
galileoguzman <galileoguzman@gmail.com>
2020-07-15 21:38:21 -0500

    Getting data from database

--- books/views.py
+++ books/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
+from .models import Book
+
 # Create your views here.
+def home_books(request):
+    books = Book.objects.all()
+    books_count = books.count()
+    return render(request, 'home_books.html',{
+        'books': books,
+        'count': books_count
+    })
