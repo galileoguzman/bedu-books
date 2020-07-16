ee5c1c9f5a1716f8c8d9c4216bba753ec6556811
galileoguzman <galileoguzman@gmail.com>
2020-07-15 20:12:58 -0500

    Added configuration for media and static files

--- books/models.py
+++ books/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+
 # Create your models here.
+class Book(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.TextField()
+    image = models.ImageField(upload_to='books')
+
+    def __str__(self):
+        return self.title
